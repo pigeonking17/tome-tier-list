1d32b6a9c2864f6ef8257d85f1fdcfafebf90152
pigeonking17 <pigeonking517@gmail.com>
2022-06-03 13:01:57 +0700

    got part of it working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
 use reqwest;
 use scraper::{Html, Selector};
 
+struct Class_table {
+    classes: Vec<Class>,
+}
+
+#[derive(Debug)]
+struct Class {
+    class: String,
+    wins: i32,
+    deaths: i32,
+    win_percent: f64,
+}
+
+impl Class_table {
+    fn new() -> Self {
+        Class_table {
+            classes: Vec::new(),
+        }
+    }
+}
+
+impl Class {
+    fn new(class: String, wins: i32, deaths: i32) -> Self {
+        let win_percent: f64 = (wins as f64 / deaths as f64) * 100f64;
+        Class {
+            class,
+            wins,
+            deaths,
+            win_percent,
+        }
+    }
+}
 
 fn main() {
     let mut page_num = 0;
+    let mut wins = 0;
 
     let url = format!("https://te4.org/characters-vault?tag_name=&tag_level_min=&tag_level_max=&tag_winner=winner&tag_difficulty[]=36&tag_class[]=34&tag_game[]=699172&page=0");
 
@@ -16,13 +48,17 @@ fn main() {
     let selector_odd = Selector::parse("tr.odd").unwrap();
     let selector_even = Selector::parse("tr.even").unwrap();
 
-    let mut elements: Vec<scraper::ElementRef> = document.select(&selector_odd).chain(document.select(&selector_even)).collect();
+    let mut elements: Vec<_> = document
+        .select(&selector_odd)
+        .chain(document.select(&selector_even))
+        .map(|x| x.inner_html())
+        .collect();
 
-    while &elements[0].inner_html() != "<td colspan=\"3\">No characters available.</td> " {
+    while &elements[0] != "<td colspan=\"3\">No characters available.</td> " {
         println!("Doing page no. {}", page_num);
 
         for element in &elements {
-            println!("{}", element.inner_html());
+            wins += 1;
         }
 
         page_num += 1;
@@ -35,19 +71,71 @@ fn main() {
         let body = resp.text().unwrap();
 
         let document = Html::parse_document(&body);
-        let selector_odd = Selector::parse("tr.odd").unwrap();
-        let selector_even = Selector::parse("tr.even").unwrap();
 
-        elements = document.select(&selector_odd).chain(document.select(&selector_even)).collect();
+        elements = document
+            .select(&selector_odd)
+            .chain(document.select(&selector_even))
+            .map(|x| x.inner_html())
+            .collect();
     }
 
-    if elements[0].inner_html().as_str() == "<td colspan=\"3\">No characters available.</td> " {
+    if elements[0].as_str() == "<td colspan=\"3\">No characters available.</td> " {
         println!("Invalid");
     }
 
-    for element in elements {
-        println!("{}", element.inner_html());
+
+    let mut page_num = 0;
+    let mut deaths = 0;
+
+    let url = format!("https://te4.org/characters-vault?tag_name=&tag_level_min=&tag_level_max=&tag_dead=dead&tag_difficulty[]=36&tag_class[]=34&tag_game[]=699172&page=0");
+
+    let resp = reqwest::blocking::get(url).unwrap();
+    assert!(resp.status().is_success());
+
+    let body = resp.text().unwrap();
+
+    let document = Html::parse_document(&body);
+    let selector_odd = Selector::parse("tr.odd").unwrap();
+    let selector_even = Selector::parse("tr.even").unwrap();
+
+    let mut elements: Vec<_> = document
+        .select(&selector_odd)
+        .chain(document.select(&selector_even))
+        .map(|x| x.inner_html())
+        .collect();
+
+    while &elements[0] != "<td colspan=\"3\">No characters available.</td> " {
+        println!("Doing page no. {}", page_num);
+
+        for element in &elements {
+            deaths += 1;
+        }
+
+        page_num += 1;
+
+        let url = format!("https://te4.org/characters-vault?tag_name=&tag_level_min=&tag_level_max=&tag_dead=dead&tag_difficulty[]=36&tag_class[]=34&tag_game[]=699172&page={}", page_num);
+
+        let resp = reqwest::blocking::get(url).unwrap();
+        assert!(resp.status().is_success());
+
+        let body = resp.text().unwrap();
+
+        let document = Html::parse_document(&body);
+
+        elements = document
+            .select(&selector_odd)
+            .chain(document.select(&selector_even))
+            .map(|x| x.inner_html())
+            .collect();
     }
+
+    if elements[0].as_str() == "<td colspan=\"3\">No characters available.</td> " {
+        println!("Invalid");
+    }
+
+    let corruptor_class = Class::new("corruptor".to_string(), wins, deaths);
+
+    println!("{:?}", corruptor_class);
 }
 
 fn class_to_num(class: &str) -> i32 {
